197301a7bd4f5d46bc405fd80f601a96df850256
hyejinKim <zlagg23@gmail.com>
2023-05-10 15:30:04 +0900

    hj

--- 올림픽/혜진.py
+++ 올림픽/혜진.py
@@ -4,12 +4,12 @@ arr = []
 for _ in range(N):
     arr.append(list(map(int, input().split())))
 
-sorted_arr = sorted(arr, key=lambda x: (x[1], x[2], x[3]))
+sorted_arr = sorted(arr, key=lambda x: (x[1], x[2], x[3]))  # 0번 인덱스 빼고 정렬
 
 
 for i in range(N):
-    if sorted_arr[i][0] == K:
-         print(N - i)
+    if sorted_arr[i][0] == K:   
+         print(N - i)   # 등수는 거꾸로
          break
     
     
